0d48d041bd2f04b61b5f9204a41ec6c9bddd6bbe
Daligz <oscardali370@gmail.com>
2022-02-25 17:04:20 -0600

    Parser id added

--- src/main/java/me/upp/parser/Test.java
+++ src/main/java/me/upp/parser/Test.java
@@ -1,8 +1,13 @@
 package me.upp.parser;
 
 import org.yuanheng.cookcc.CookCCOption;
+import org.yuanheng.cookcc.Lex;
 
 @CookCCOption(lexerTable = "compressed", parserTable = "compressed")
 public class Test extends Parser {
 
+    @Lex(pattern = "([a-zA-Z]+)|([0-9]+)", token = "ID")
+    protected String parseId() {
+        return yyText();
+    }
 }
