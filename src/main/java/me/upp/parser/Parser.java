938338e068240e7df7f1fbcb4474b841d10dff49
Daligz <oscardali370@gmail.com>
2022-02-25 16:06:37 -0600

    Project cleaned

--- src/main/java/me/upp/parser/Parser.java
+++ src/main/java/me/upp/parser/Parser.java
@@ -1,12 +1,10 @@
 package me.upp.parser;
 
-import com.github.javaparser.JavaParser;
-
 public class Parser {
 
     public static final String EXPRESSION = "id+id-id=id+id-id/id";
 
     public static void main(final String[] args) {
-        new Test().compute(new JavaParser(), Parser.EXPRESSION);
+
     }
 }
--- src/main/java/me/upp/parser/Test.java
+++ src/main/java/me/upp/parser/Test.java
@@ -1,21 +1,8 @@
 package me.upp.parser;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParseResult;
-import com.github.javaparser.ParseStart;
-import com.github.javaparser.ast.CompilationUnit;
-
-import static com.github.javaparser.Providers.provider;
-
 public class Test {
 
-    public void compute(final JavaParser javaParser, final String expression) {
-        final ParseResult<CompilationUnit> parse = javaParser.parse(ParseStart.COMPILATION_UNIT, provider(expression));
-        if (!(parse.isSuccessful())) {
-            System.out.println("chale");
-            System.out.println(parse.getProblems().toString());
-        } else {
-            parse.ifSuccessful(System.out::println);
-        }
+    public void compute(final String expression) {
+
     }
 }
