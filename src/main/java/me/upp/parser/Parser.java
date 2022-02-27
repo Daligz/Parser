b4a533eeea4e7b58ed2dabdd5545ab994c8255b3
Daligz <oscardali370@gmail.com>
2022-02-26 21:53:03 -0600

    Generated files added and default structure implemented

--- src/main/java/me/upp/parser/Parser.java
+++ src/main/java/me/upp/parser/Parser.java
@@ -1,8 +1,20 @@
 package me.upp.parser;
 
+import com.github.jhoenicke.javacup.runtime.Symbol;
+import me.upp.parser.generated.Syntax;
+
 public class Parser {
 
     public static void main(final String[] args) {
-        
+        final Syntax syntax = new Syntax();
+        try {
+            syntax.parse();
+        } catch (final Exception exception) {
+            exception.printStackTrace();
+            final Symbol symbol = syntax.getSymbol();
+            System.out.println("Line: " + (symbol.right + 1));
+            System.out.println("Column: " + (symbol.left + 1));
+            System.out.println("Value: " + symbol.value);
+        }
     }
 }
