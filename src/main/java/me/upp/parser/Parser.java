4033f36bbc3b7649ec9672fddf71c57be62b043a
Daligz <oscardali370@gmail.com>
2022-03-02 19:17:40 -0600

    Cleanup

--- src/main/java/me/upp/parser/Parser.java
+++ src/main/java/me/upp/parser/Parser.java
@@ -1,24 +1,8 @@
 package me.upp.parser;
 
-import com.github.jhoenicke.javacup.runtime.Symbol;
-import me.upp.parser.generated.LexerCup;
-import me.upp.parser.generated.Syntax;
-
-import java.io.StringReader;
-
 public class Parser {
 
     public static void main(final String[] args) {
-        final Syntax syntax = new Syntax(new LexerCup(new StringReader("id + id - id = id + id - id / id")));
-        try {
-            syntax.parse();
-            System.out.println("Successful entry!");
-        } catch (final Exception exception) {
-            exception.printStackTrace();
-            final Symbol symbol = syntax.getSymbol();
-            System.out.println("Line: " + (symbol.right + 1));
-            System.out.println("Column: " + (symbol.left + 1));
-            System.out.println("Value: " + symbol.value);
-        }
+
     }
 }
