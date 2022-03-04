9f0102db773e0bea7594334219230c60ed3f47cc
Daligz <oscardali370@gmail.com>
2022-03-03 19:01:00 -0600

    Expressions implemented successfully

--- src/main/java/me/upp/parser/Parser.java
+++ src/main/java/me/upp/parser/Parser.java
@@ -1,8 +1,11 @@
 package me.upp.parser;
 
+import me.upp.parser.lexical.Worker;
+
 public class Parser {
 
     public static void main(final String[] args) {
-
+        final Worker worker = new Worker("id+id-id=id+id-id/id");
+        worker.compute();
     }
 }
--- src/main/java/me/upp/parser/lexical/Worker.java
+++ src/main/java/me/upp/parser/lexical/Worker.java
@@ -1,6 +1,10 @@
 package me.upp.parser.lexical;
 
 import lombok.AllArgsConstructor;
+import me.upp.parser.lexical.expressions.Expressions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @AllArgsConstructor
 public class Worker {
@@ -8,14 +12,19 @@ public class Worker {
     private final String expression;
 
     public void compute() {
-
+        for (final Pattern pattern : Expressions.PATTERNS) {
+            final Matcher matcher = pattern.matcher(this.expression);
+            while (matcher.find()) {
+                System.out.println(matcher.group());
+            }
+        }
     }
 
     public void print() {
 
     }
 
-    public void Check() {
+    public void check() {
 
     }
 }
