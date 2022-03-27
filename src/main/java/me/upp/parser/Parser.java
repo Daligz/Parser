e09500737f69045d437b2f2c96b26c751c42f6d7
Daligz <oscardali370@gmail.com>
2022-03-27 09:50:56 -0600

    Fix scanner

--- src/main/java/me/upp/parser/Parser.java
+++ src/main/java/me/upp/parser/Parser.java
@@ -11,7 +11,7 @@ public class Parser {
 
     public static void main(final String[] args) {
         System.out.print("Expresion: ");
-        final String expression = new Scanner(System.in).next().replace(" ", "");
+        final String expression = new Scanner(System.in).nextLine();
         final LexicalWorker lexicalWorker = new LexicalWorker(expression);
         lexicalWorker.compute();
         lexicalWorker.print();
